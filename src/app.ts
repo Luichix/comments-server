4dff43900ca324b652c1203817d8b2942764e2bd
Luichix <luisreynaldo.pch@gmail.com>
2023-10-28 15:12:36 -0600

    initialize project

---
+++ src/app.ts
@@ -0,0 +1,51 @@
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import config from 'config'
+import responseTime from 'response-time'
+// import connect from './utils/connect'
+import logger from './utils/logger'
+// import routes from './routes'
+import deserializeUser from './middleware/deserializeUser'
+import { restResponseTimeHistogram, startMetricsServer } from './utils/metrics'
+import swaggerDocs from './utils/swagger'
+
+const port = config.get<number>('port')
+
+const app = express()
+
+app.use(express.static('build'))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
+
+app.use(deserializeUser)
+
+app.use(
+  responseTime((req: Request, res: Response, time: number) => {
+    if (req?.route?.path) {
+      restResponseTimeHistogram.observe(
+        {
+          method: req.method,
+          route: req.route.path,
+          status_code: res.statusCode
+        },
+        time * 1000
+      )
+    }
+  })
+)
+app.get('/', (_: Request, res: Response) => {
+  res.send('Hello World')
+})
+
+app.listen(port, async () => {
+  logger.info(`🚀 Server running on port http://localhost:${port}`)
+
+  // await connect()
+
+  // routes(app)
+
+  startMetricsServer()
+
+  swaggerDocs(app, port)
+})
